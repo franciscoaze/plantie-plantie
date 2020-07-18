ec600516068483201c8ff35967e68d128f343e48
franciscoaze <franciscoaze@yahoo.com>
2020-07-18 21:47:18 +0100

    fix cmd line args

--- configuration/vars.py
+++ configuration/vars.py
@@ -1,2 +1,2 @@
 DATABASE_URL = r"postgres://kquyqdfq:NbPlwIBv66TllqtVTlgjQUMtvBIaYfa6@balarama.db.elephantsql.com:5432/kquyqdfq"
-LOG_FILE = "logs/daily_log.txt"
+LOG_FILE = "logs/system_activity.log"
--- logger.py
+++ logger.py
@@ -2,11 +2,13 @@ import os
 import time
 from database import insert_log
 from configuration.vars import LOG_FILE
+import sys
 
 msg_dict = {True: "[DB]", False: ""}
 
 
 def log_string(input_string, msg_type="SYSTEM INFO"):
+    print(f"Logging {msg_type}: {input_string}")
     new_log = {"type": msg_type, "msg": input_string}
     success = insert_log(new_log)
     with open(LOG_FILE, 'a') as file:
@@ -15,4 +17,4 @@ def log_string(input_string, msg_type="SYSTEM INFO"):
 
 
 if __name__ == '__main__':
-    log_string('Alive')
+    log_string(sys.argv[1:][0])
