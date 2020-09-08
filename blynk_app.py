b1500fa44c621cd5d17d0ef996427ac2e86b8767
franciscoaze <franciscoaze@yahoo.com>
2020-09-08 19:23:55 +0100

    added test blynk app

---
+++ blynk_app.py
@@ -0,0 +1,35 @@
+# Programa: Blynk com Raspberry Pi
+# Autor: Arduino e Cia
+import BlynkLib
+import RPi.GPIO as GPIO
+from gpiozero import CPUTemperature
+
+GPIO.setmode(GPIO.BCM)
+# Define as GPIOs 14 e 18 como saida
+GPIO.setup(17, GPIO.OUT)
+# Inicializa Blynk
+blynk = BlynkLib.Blynk('83pa6ghaq1G40yxJrxqeOLAWFV9YTRN6')
+
+
+# Registra os pinos virtuais
+@blynk.VIRTUAL_WRITE(1)
+def my_write_handler(value):
+    print('Valor de V1: {}'.format(value[0]))
+    # Acende ou apaga o led vermelho, dependendo
+    # do valor recebido
+    if value[0] >= "1":
+        GPIO.output(17,GPIO.HIGH)
+    else:
+        GPIO.output(17, GPIO.LOW)
+
+
+@blynk.VIRTUAL_READ(3)
+def my_read_handler():
+    # Envia o valor da temperatura da CPU
+    cpu = CPUTemperature()
+    print(cpu.temperature)
+    blynk.virtual_write(2, cpu.temperature)
+
+
+while True:
+    blynk.run()
