11282d62219b7ab754ec8505609a42c5c02624ab
franciscoaze <franciscoaze@yahoo.com>
2020-09-08 20:22:26 +0100

    video streaming

--- video_streaming.py
+++ video_streaming.py
@@ -43,7 +43,7 @@ def get_frames(frameCount):
         # read the next frame from the video stream, resize it,
         # convert the frame to grayscale, and blur it
         frame = vs.read()
-        frame = imutils.resize(frame, width=400)
+        # frame = imutils.resize(frame, width=400)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         # gray = cv2.GaussianBlur(gray, (7, 7), 0)
         # grab the current timestamp and draw it on the frame
@@ -53,26 +53,6 @@ def get_frames(frameCount):
                     cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
         total += 1
-        # if the total number of frames has reached a sufficient
-        # number to construct a reasonable background model, then
-        # continue to process the frame
-        # if total > frameCount:
-            # detect motion in the image
-            # motion = md.detect(gray)
-            # check to see if motion was found in the frame
-            # if motion is not None:
-            #     # unpack the tuple and draw the box surrounding the
-            #     # "motion area" on the output frame
-            #     (thresh, (minX, minY, maxX, maxY)) = motion
-            #     cv2.rectangle(frame, (minX, minY), (maxX, maxY),
-            #                   (0, 0, 255), 2)
-            # update the background model and increment the total number
-            # of frames read thus far
-            # md.update(gray)
-
-
-        # acquire the lock, set the output frame, and release the
-        # lock
         with lock:
             outputFrame = frame.copy()
 
