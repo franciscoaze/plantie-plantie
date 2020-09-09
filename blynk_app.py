91154007b39e3c2a356e7b9b9fef68412676ea05
franciscoaze <franciscoaze@yahoo.com>
2020-09-09 19:29:58 +0100

    blynk tests

--- blynk_app.py
+++ blynk_app.py
@@ -10,10 +10,6 @@ GPIO.setup(17, GPIO.OUT)
 # Inicializa Blynk
 blynk = BlynkLib.Blynk('83pa6ghaq1G40yxJrxqeOLAWFV9YTRN6')
 
-@blynk.handle_event("connect")
-def connect_handler():
-    print("Connected to device")
-
 # Registra os pinos virtuais
 @blynk.VIRTUAL_WRITE(1)
 def my_write_handler(value):
@@ -25,6 +21,16 @@ def my_write_handler(value):
     else:
         GPIO.output(17, GPIO.LOW)
 
+# Camera ON/OFF
+@blynk.VIRTUAL_WRITE(0)
+def my_write_handler(value):
+    print('Valor de V0: {}'.format(value[0]))
+    # Acende ou apaga o led vermelho, dependendo
+    # do valor recebido
+    if value[0] >= "1":
+        GPIO.output(17,GPIO.HIGH)
+    else:
+        GPIO.output(17, GPIO.LOW)
 
 @blynk.VIRTUAL_READ(2)
 def my_read_handler():
--- video_streaming.py
+++ video_streaming.py
@@ -10,93 +10,105 @@ import imutils
 import time
 import cv2
 
-# initialize the output frame and a lock used to ensure thread-safe
-# exchanges of the output frames (useful when multiple browsers/tabs
-# are viewing the stream)
-outputFrame = None
-lock = threading.Lock()
-# initialize a flask object
-app = Flask(__name__)
-# initialize the video stream and allow the camera sensor to
-# warmup
-vs = WebcamVideoStream(src=-1).start()
-time.sleep(2.0)
 
 
-@app.route("/")
-def index():
-    # return the rendered template
-    return render_template("index.html")
+class VideoStreamer:
+	outputFrame = None
+	lock = None
+	app = None
+	vs = None
 
+	PORT = '8000'
 
-def get_frames(frameCount):
-    # grab global references to the video stream, output frame, and
-    # lock variables
-    global vs, outputFrame, lock
-    total = 0
-    # loop over frames from the video stream
-    while True:
-        # read the next frame from the video stream, resize it,
-        # convert the frame to grayscale, and blur it
-        frame = vs.read()
-        # frame = imutils.resize(frame, width=400)
-        # grab the current timestamp and draw it on the frame
-        timestamp = datetime.datetime.now()
-        cv2.putText(frame, timestamp.strftime(
-            "%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+	def start_new_stream(self):
+		# initialize the output frame and a lock used to ensure thread-safe
+		# exchanges of the output frames (useful when multiple browsers/tabs
+		# are viewing the stream)
+		self.outputFrame = None
+		self.lock = threading.Lock()
+		# initialize a flask object
+		self.app = Flask('video_stream')
+		# initialize the video stream and allow the camera sensor to warmup
+		self.vs = WebcamVideoStream(src=-1).start()
+		time.sleep(2.0)
 
-        total += 1
-        with lock:
-            outputFrame = frame.copy()
+		t = threading.Thread(target=self.get_frames, args=(32,))
+		t.daemon = True
+		t.start()
+		# start the flask app
+		self.app.run(host='0.0.0.0', port=self.PORT, debug=True, threaded=True, use_reloader=False)
 
+	def stop_stream(self):
+		# release the video stream pointer
+		self.vs.stop()
 
-def generate():
-    # grab global references to the output frame and lock variables
-    global outputFrame, lock
-    # loop over frames from the output stream
-    while True:
-        # wait until the lock is acquired
-        with lock:
-            # check if the output frame is available, otherwise skip
-            # the iteration of the loop
-            if outputFrame is None:
-                continue
-            # encode the frame in JPEG format
-            (flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
-            # ensure the frame was successfully encoded
-            if not flag:
-                continue
-        # yield the output frame in the byte format
-        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
+	def get_frames(self):
+		# grab global references to the video stream, output frame, and
+		# lock variables
+		total = 0
+		# loop over frames from the video stream
+		while True:
+			# read the next frame from the video stream, resize it,
+			# convert the frame to grayscale, and blur it
+			frame = self.vs.read()
+			# frame = imutils.resize(frame, width=400)
+			# grab the current timestamp and draw it on the frame
+			timestamp = datetime.datetime.now()
+			cv2.putText(frame, timestamp.strftime(
+				"%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
+						cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
+			total += 1
+			with self.lock:
+				self.outputFrame = frame.copy()
 
-@app.route("/video_feed")
-def video_feed():
-    # return the response generated along with the specific media
-    # type (mime type)
-    return Response(generate(),
-                    mimetype="multipart/x-mixed-replace; boundary=frame")
+	def generate(self):
+		# grab global references to the output frame and lock variables
+		# loop over frames from the output stream
+		while True:
+			# wait until the lock is acquired
+			with self.lock:
+				# check if the output frame is available, otherwise skip
+				# the iteration of the loop
+				if self.outputFrame is None:
+					continue
+				# encode the frame in JPEG format
+				(flag, encodedImage) = cv2.imencode(".jpg", self.outputFrame)
+				# ensure the frame was successfully encoded
+				if not flag:
+					continue
+			# yield the output frame in the byte format
+			yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
 
+	@app.route("/video_feed")
+	def video_feed(self):
+		# return the response generated along with the specific media
+		# type (mime type)
+		return Response(self.generate(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
-# check to see if this is the main thread of execution
-if __name__ == '__main__':
-    # construct the argument parser and parse command line arguments
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--ip", type=str, required=True,
-                    help="ip address of the device")
-    ap.add_argument("-o", "--port", type=int, required=True,
-                    help="ephemeral port number of the server (1024 to 65535)")
-    ap.add_argument("-f", "--frame-count", type=int, default=32,
-                    help="# of frames used to construct the background model")
-    args = vars(ap.parse_args())
-    # start a thread that will perform motion detection
-    t = threading.Thread(target=get_frames, args=(
-        args["frame_count"],))
-    t.daemon = True
-    t.start()
-    # start the flask app
-    app.run(host=args["ip"], port=args["port"], debug=True,
-            threaded=True, use_reloader=False)
-# release the video stream pointer
-vs.stop()
+# @app.route("/")
+# def index():
+#     # return the rendered template
+#     return render_template("index.html")
+
+#
+# # check to see if this is the main thread of execution
+# if __name__ == '__main__':
+#     # construct the argument parser and parse command line arguments
+#     ap = argparse.ArgumentParser()
+#     ap.add_argument("-i", "--ip", type=str, required=True,
+#                     help="ip address of the device")
+#     ap.add_argument("-o", "--port", type=int, required=True,
+#                     help="ephemeral port number of the server (1024 to 65535)")
+#     ap.add_argument("-f", "--frame-count", type=int, default=32,
+#                     help="# of frames used to construct the background model")
+#     args = vars(ap.parse_args())
+#     # start a thread that will perform motion detection
+#     t = threading.Thread(target=get_frames, args=(32,))
+#     t.daemon = True
+#     t.start()
+#     # start the flask app
+#     app.run(host=args["ip"], port=args["port"], debug=True,
+#             threaded=True, use_reloader=False)
+# # release the video stream pointer
+# vs.stop()
