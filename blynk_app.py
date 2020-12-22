df5b22c17f187624747772f9bb2ccf025d63ebb4
franciscoaze <franciscoaze@yahoo.com>
2020-12-22 00:27:41 +0000

    video_stream almost

--- blynk_app.py
+++ blynk_app.py
@@ -29,12 +29,14 @@ def control_video_stream(value):
     print('Valor de V0: {}'.format(value[0]))
     # Acende ou apaga o led vermelho, dependendo
     # do valor recebido
-    if value[0] >= "1":
-        r = requests.get(url = URL+'/video_on')
-    else:
-        r = requests.get(url=URL + '/video_off')
-    print(r)
-
+    try:
+        if value[0] >= "1":
+            r = requests.get(url = URL+'/video_on')
+        else:
+            r = requests.get(url=URL + '/video_off')
+        print(r)
+    except:
+        print('no response')
 @blynk.VIRTUAL_READ(2)
 def my_read_handler():
     # Envia o valor da temperatura da CPU
