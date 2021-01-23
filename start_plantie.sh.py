9e14659674b6a0cf70d5ce3cf7f51b65786e9054
franciscopi <franciscoaze@yahoo.com>
2021-01-23 20:27:38 +0000

    changes to sh

--- start_plantie.sh.py
+++ start_plantie.sh.py
@@ -1,5 +1,6 @@
 #!/bin/bash
+projects_dir="/home/pi/Desktop/Projects"
 sudo service mosquitto stop
-mosquitto
-lxterminal --command="bash -c 'source .venv/bin/activate; cd plantie-plantie; python -m pyplantie.arduino_rwi; bash'"
-lxterminal --command="bash -c 'source .venv/bin/activate; cd plantie-plantie; python -m pyplantie.raspberry_rwi; bash'"
+mosquitto &
+lxterminal --command="bash -c 'source $projects_dir/.venv/bin/activate; cd plantie-plantie; python -m pyplantie.arduino_rwi; bash'" &
+lxterminal --command="bash -c 'source $projects_dir/.venv/bin/activate; cd plantie-plantie; python -m pyplantie.raspberry_rwi; bash'" &
