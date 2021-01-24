01a28c479cdd1a28988716d3acbb309e1dbda509
franciscoaze <franciscoaze@yahoo.com>
2021-01-24 20:23:54 +0000

    fixing local server

--- pyplantie/utils/constants.py
+++ pyplantie/utils/constants.py
@@ -6,7 +6,7 @@ PUMP_SECONDS = os.getenv("PUMP_SECONDS", 3)
 
 BROKER_ADDRESS = os.getenv("BROKER_ADDRESS", "192.168.1.80")
 
-ARDUINO_USB_PORT = os.getenv("ARDUINO_USB_PORT",'/dev/ttyUSB0')
+ARDUINO_USB_PORT = os.getenv("ARDUINO_USB_PORT", '/dev/ttyUSB0')
 ARDUINO_ACTUATORS_FILE = os.getenv("ARDUINO_ACTUATORS_FILE", "pyplantie.workers.arduino_actuators")
 ARDUINO_SENSORS_FILE = os.getenv("ARDUINO_SENSORS_FILE", "pyplantie.workers.arduino_sensors")
 ARDUINO_CLIENT_NAME = os.getenv("ARDUINO_CLIENT_NAME", "Arduino-RWI")
