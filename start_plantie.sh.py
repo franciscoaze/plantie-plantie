d3d0e99d0c6312a4f1fa6715279977e344ae4391
franciscoaze <franciscoaze@yahoo.com>
2021-01-24 20:57:36 +0000

    fixing local server

--- start_plantie.sh.py
+++ start_plantie.sh.py
@@ -4,3 +4,5 @@ sudo service mosquitto stop
 mosquitto &
 lxterminal --command="bash -c 'source $projects_dir/.venv/bin/activate; cd $projects_dir/plantie-plantie; python -m pyplantie.arduino_rwi; bash'" &
 lxterminal --command="bash -c 'source $projects_dir/.venv/bin/activate; cd $projects_dir/plantie-plantie; python -m pyplantie.raspberry_rwi; bash'" &
+lxterminal --command="bash -c 'source $projects_dir/.venv/bin/activate; cd $projects_dir/plantie-plantie; python -m pyplantie.core bash'" &
+lxterminal --command="bash -c 'source $projects_dir/.venv/bin/activate; cd $projects_dir/plantie-plantie; python -m pyplantie.blynk_app bash'" &
