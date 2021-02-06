990f01b689fe10859abab2f7c77fd74f0db33bc5
franciscoaze <franciscoaze@yahoo.com>
2021-02-06 22:48:54 +0000

    added job menu to blynk_app.py

--- pyplantie/utils/table_models.py
+++ pyplantie/utils/table_models.py
@@ -1,5 +1,5 @@
 
 class Jobs(object):
     name = "JOBS"
-    create_columns = "update_time TIMESTAMPTZ default current_timestamp, name_id text PRIMARY KEY, trigger_args text, value text, value_legend text"
-    columns = "update_time, name_id, trigger_args, value, value_legend"
+    create_columns = "update_time TIMESTAMPTZ default current_timestamp, name_id text PRIMARY KEY, trigger_args text, value text, value_legend text, trigger text"
+    columns = "update_time, name_id, trigger_args, value, value_legend, trigger"
