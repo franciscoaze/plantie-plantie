950a8176666380065375c72691014c7dc2be5d6f
franciscoaze <franciscoaze@yahoo.com>
2021-02-06 20:22:21 +0000

    added table support to blynk_app.py

--- pyplantie/blynk_app.py
+++ pyplantie/blynk_app.py
@@ -187,7 +187,7 @@ def update_table(value):
             idx += 1
 
 
-@blynk.on(BlynkLib.BlynkProtocol.connect)
+@blynk.on("connected")
 def connect_handler():
     print("Blynk is connected heyyy.")
     blynk.setProperty(13, "labels", "Menu Item 1", "Menu Item 2", "Menu Item 3");
